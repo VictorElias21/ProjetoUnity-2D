09fb50a3ac145724b255ed6748cd27f7e454446f
VictorElias21 <itz.victor.elias@gmail.com>
2018-08-14 10:54:29 -0300

    Aula 12: Plataforma não está movendo

---
+++ Assets/Scripts/MovingPlatformCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformCtrl : MonoBehaviour {
+
+	public Transform Pos1, Pos2;
+
+	public float speed = 2f;
+
+	public float waitTime = 0.5f;
+
+	Vector3 nextPos;
+	void Start () {
+		nextPos = Pos1.position;
+	}
+
+	IEnumerable Move(){
+		while (true){
+			if (transform.position == Pos1.position){
+				nextPos = Pos2.position;
+				yield return new WaitForSeconds(waitTime);
+			}
+			if (transform.position == Pos2.position){
+				nextPos = Pos1.position;
+				yield return new WaitForSeconds(waitTime);
+			}
+			transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawLine(Pos1.position, Pos2.position);
+	}
+}
